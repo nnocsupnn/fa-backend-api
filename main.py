34bedbf2711b546a9a75d2617871327c1a498cff
nnocsupnn <nincas21@gmail.com>
2023-05-14 03:09:31 +0800

    chore: enhancement on class importing

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from fastapi import FastAPI, Depends
 import uvicorn
-import datetime
-from fastapi_jwt_auth import AuthJWT
-from fastapi_jwt_auth.exceptions import AuthJWTException
-
-from components.db import Base, SessionLocal, engine, User, UserDetail, Occupation, Incomes, IncomeProtection, IncomeProtectionProvision, Dependencies, DependencyDetail, DependencyProvision, Expenses, TextTemplate
+from fastapi import FastAPI, Depends
+from components.db import Base, SessionLocal, engine
 from components.functions import nullCheckingResponse
-from routes import UserAPI, AuthAPI, TestAPI, UserDetailAPI
+from routes import *
 from security.jwt_auth import AuthSecurity
+
 
 # Intialize db (DDL)
 Base.metadata.create_all(engine, checkfirst=True)
@@ -23,11 +20,25 @@ app = authSecurity.getApp()
 # # # # # # # #
 #  Routes     #
 # # # # # # # #
+'''
+After creating the routes services, you need to add the API Class here under resource var 
+to automatically include in the registered routes
+'''
+resource = [UserAPI, UserDetailAPI]
 
+'''
+Exception routes for auth and other special cases
+just to not include the auth module
+'''
 app.include_router(TestAPI(SessionLocal).router)
 app.include_router(AuthAPI(SessionLocal).router)
-app.include_router(UserAPI(SessionLocal).router, dependencies=[Depends(authSecurity.auth_user)])
-app.include_router(UserDetailAPI(SessionLocal).router, dependencies=[Depends(authSecurity.auth_user)])
+
+'''
+Run all API Classes to registered to our router
+'''
+for instance in resource:
+    app.include_router(instance(SessionLocal).router, dependencies=[Depends(authSecurity.auth_user)])
+
 
 # Start Server
 if __name__ == "__main__":
